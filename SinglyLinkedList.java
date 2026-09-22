b07fb44bab599c3d05ac3b78a419cd9ea66a9742
Josh <191194699+FriedFliBo@users.noreply.github.com>
2026-09-22 16:20:52 +0800

    this fails double cases

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -8,63 +8,63 @@ public class SinglyLinkedList<E extends Comparable<E>> {
     private static class Node<E> {
         private E element;
         private Node<E> next;
-    
-        public Node(E e, Node<E> n){
+
+        public Node(E e, Node<E> n) {
             element = e;
             next = n;
         }
-    
-        public E getElement(){
+
+        public E getElement() {
             return element;
         }
-    
-        public Node<E> getNext(){
+
+        public Node<E> getNext() {
             return next;
         }
-    
-        public void setNext(Node<E> n){
+
+        public void setNext(Node<E> n) {
             next = n;
         }
     }
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {
 
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public E first(){
-        if (isEmpty()){
+    public E first() {
+        if (isEmpty()) {
             return null;
-        } 
+        }
         return head.getElement();
     }
 
-    public E last(){
-        if (isEmpty()){
+    public E last() {
+        if (isEmpty()) {
             return null;
         }
         return tail.getElement();
     }
 
-    public void addFirst(E e){
+    public void addFirst(E e) {
         head = new Node<>(e, head);
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = head;
         }
         size++;
     }
 
-    public void addLast(E e){
+    public void addLast(E e) {
         Node<E> newest = new Node<>(e, null);
-        if (isEmpty()){
+        if (isEmpty()) {
             head = newest;
         } else {
             tail.setNext(newest);
@@ -73,8 +73,8 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         size++;
     }
 
-    public E removeFirst(){
-        if (isEmpty()){
+    public E removeFirst() {
+        if (isEmpty()) {
             return null;
         }
 
@@ -82,13 +82,13 @@ public class SinglyLinkedList<E extends Comparable<E>> {
         head = head.getNext();
         size--;
 
-        if (isEmpty()){
+        if (isEmpty()) {
             tail = null;
         }
         return answer;
     }
 
-    public String toString(){
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         Node<E> current = head;
         while (current != null) {
@@ -100,10 +100,77 @@ public class SinglyLinkedList<E extends Comparable<E>> {
     }
 
     // write your codes here
-    public void swap(){
+    public void swap() {
+        int size = size();
         
+        // store all the nodes in an array
+        Node nodeList[] = new Node[size];
 
+        int sortedList[] = new int[size];
+        int indx = 0;
+
+        Node curr = this.head;
+        while (curr != null) {
+            nodeList[indx] = curr;
+            sortedList[indx++] = (Integer) curr.getElement();
+            curr = curr.getNext();
+        }
+
+        // keep track of max and min
+        bubbleSort(sortedList);
+        int left = 0;
+        int right = size() - 1;
+        
+        while (left < right) {
+            int min = sortedList[left];
+            int max = sortedList[right];
+
+            int indx1 = 0;
+            int indx2 = 0;
+            while ((Integer) nodeList[indx1].getElement() != min) {
+                indx1++;
+            }
+
+            while ((Integer) nodeList[indx2].getElement() != max) {
+                indx2++;
+            }
+
+            Node temp = nodeList[indx1];
+            nodeList[indx1] = nodeList[indx2];
+            nodeList[indx2] = temp;
+
+            left++;
+            right--;
+        }
+
+        indx = 0;
+        head = nodeList[indx];
+        Node ptr = head;
+        for (int i = 1; i < size(); i++) {
+            ptr.setNext(nodeList[indx + i]);
+            ptr = ptr.getNext();
+        }
+
+        tail = ptr;
+        tail.setNext(null);
+        ptr = head;
+        // while (ptr != null) {
+
+        // }
     }
-   
-}
 
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
